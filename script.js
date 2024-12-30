7262d12d557d71a375a494503a4d616eaaebf539
nextmove96 <herenextmove@gmail.com>
2024-12-30 12:17:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function redirectToWhatsApp() {
-    const phone = "9226418410";
+    const phone = "8987726266";
     const message = "I want to make a tournament application";
     window.location.href = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 }
 function redirectToEmail() {
-    const email = "mailto:tournamentappseller@gmail.com?subject=Tournament Application&body=I want to make a tournament application.";
+    const email = "mailto:herenextmove@gmail.com@gmail.com?subject=Tournament Application&body=I want to make a tournament application.";
     window.location.href = email;
 }
 function redirectToPhone() {
-    const phone = "tel:9226418410";
+    const phone = "tel:8987726266";
     window.location.href = phone;
 }
 function toggleMenu() {
